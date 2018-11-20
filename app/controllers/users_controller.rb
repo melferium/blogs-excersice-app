9b40134232bff094ffb4d5fdf0665751a5097fbc
melferium <ferdinand.melchor@hotmail.com>
2018-11-21 01:14:54 +0800

    complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   
   def new
     @user = User.new
@@ -50,5 +51,11 @@ class UsersController < ApplicationController
   end
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only modify your own account"
+      redirect_to root_path
+    end
   end
 end
