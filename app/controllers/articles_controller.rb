b7632cbbe5ee9f79664351f497234868da95b862
melferium <ferdinand.melchor@hotmail.com>
2018-11-20 10:49:56 +0800

    add access restriction in view layer

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Successfully created new article"
       redirect_to articles_path
